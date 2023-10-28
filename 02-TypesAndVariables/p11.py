054801982504e8a2694ec444e4061da8f44ceb4f
Kkmil <kamilkufel3@gmail.com>
2023-10-28 13:49:17 +0200

    28.10

---
+++ 02-TypesAndVariables/p11.py
@@ -0,0 +1,7 @@
+father_income = 5450
+mother_income = 4920
+number_of_people = 5
+Total_income = father_income + mother_income
+income_per_person = Total_income/5
+print(f"Total income: {Total_income}")
+print(f"Income per person: {income_per_person}")
---
+++ 02-TypesAndVariables/p9.py
@@ -0,0 +1,15 @@
+n1 = 5
+n2 = 1
+n3 = 8
+n4 = 6
+n5 = 3
+
+sum = n1 + n2 + n3 + n4 + n5
+sum_squared = n1 ** 2 + n2 ** 2 + n3 ** 2 + n4 ** 2 + n5 ** 2
+quotient = n3 / n5
+
+message = n3 == n4
+print("Sum of variables", sum)
+print("Squared sum:", sum_squared)
+print("quotient", quotient)
+print("message:", message)
